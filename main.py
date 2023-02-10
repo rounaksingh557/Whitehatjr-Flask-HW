a0be8cee7d668a97541529e097afd8703990bb76
Rounak Singh <rounaksingh9431738764@gmail.com>
2023-02-10 21:58:44 +0530

    Completed Home Work

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ app: Flask = Flask(__name__)
 
 
 @app.route('/')
-def index() -> str:
+def home() -> str:
     return render_template('index.html')
+
+
+@app.route('/me/', endpoint="aboutMe")
+def aboutMe() -> str:
+    return render_template('aboutMe.html')
+
+
+@app.route('/Mom/', endpoint="Mom")
+def aboutMe() -> str:
+    return render_template('mom.html')
+
+
+@app.route('/dad/', endpoint="Dad")
+def aboutMe() -> str:
+    return render_template('dad.html')
 
 
 if __name__ == "__main__":
